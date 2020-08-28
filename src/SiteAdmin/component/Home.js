c1767efe87e38a92ec78e6624b449fe77ad697e2
PratilipiAich <pratilipiaich@gmail.com>
2020-08-28 10:10:08 +0530

    Modified create account button for Site Admin

--- src/SiteAdmin/component/Home.js
+++ src/SiteAdmin/component/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import GoogleLogin from "react-google-login";
+//import { GoogleLogin } from "react-google-button"
 import "../../App.css";
 import Navbar from "./Navbar";
-import {Card, CardContent} from "@material-ui/core"
+import {Card, CardContent, TextareaAutosize, Button} from "@material-ui/core"
 import {Container} from "@material-ui/core"
-import { Typography, Divider} from "@material-ui/core";
+import { Typography, Divider,Grid } from "@material-ui/core";
 import Header from '../../Scorer/Header'
 import Cookies from 'js-cookie'
 import {Redirect} from "react-router-dom";
@@ -17,7 +18,7 @@ class Home extends Component {
 
   render() {
   const user_name = Cookies.get("name");
-  if(Cookies.get("role") === undefined || Cookies.get("role") !== "CABI_SITE_ADMIN") return <Redirect to  = "/" /> 
+  if(Cookies.get("role") === undefined || Cookies.get("role") != "CABI_SITE_ADMIN") return <Redirect to  = "/" /> 
     return (
     
       <div style = {{marginTop:80}}>
@@ -33,8 +34,28 @@ class Home extends Component {
         <Typography>
           Click here to create a new Google Account
         </Typography>
+        <br/>
         <Divider/>
-       
+        <Button
+            className = "btn newBtn"
+              variant="contained"
+              style={{
+                
+                border: "none",
+                color: "white",
+                padding: "08px 20px",
+                fontSize: "medium",
+                cursor: "pointer",
+                marginTop: "20px",
+                backgroundColor: "#3f51b5",
+               
+              }}
+              href = "https://accounts.google.com/signup/v2/webcreateaccount?hl=en&flowName=GlifWebSignIn&flowEntry=SignUp"
+              target = "_blank"
+>
+              Create New Account
+            </Button>
+{/* 
         <GoogleLogin
           className="google-create-account"
           clientId="402744950664-cefekape7t5m71d9ok33fun1pg5hgdb7.apps.googleusercontent.com"
@@ -43,7 +64,7 @@ class Home extends Component {
           onFailure={this.responseGoogle}
           isSignedIn={true}
           cookiePolicy={"single_host_origin"}
-        /></CardContent></Card></Container>
+        /> */}</CardContent></Card></Container>
        
         <h1
           style={{
